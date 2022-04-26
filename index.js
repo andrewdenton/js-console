14c8074232d0321b399bd33f2d2f6e0044c7746c
Andy Denton <andrew.denton@sky.uk>
2022-04-26 17:41:23 +0100

    Tweaked timer demo code

--- index.js
+++ index.js
@@ -58,11 +58,12 @@ function groupDemo() {
 
 function aSlowishFunction() {
   console.time('Time');
-  for (let i=0; i<10000000000; i++) {
-    if (i % 1000000000 === 0) {
+  for (let i=0; i<1000000000; i++) {
+    if (i % 100000000 === 0) {
       console.timeLog('Time');
     }
   };
+  console.timeEnd("Time");
 }
 
 async function displayDataInstrumented() {
